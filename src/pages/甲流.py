3efabc0b2442384d8f4ff6d7ed9ba9b0d671943d
VVVHarry <github@aal>
2026-06-28 00:14:06 +0800

    chore: remove stray .py files

--- src/pages/甲流.py
+++
@@ -1,9 +0,0 @@
-n=int(input())
-total = 0
-
-for i in range(n):
-    name, temp, cough = input().split()
-    if float(temp)>=37.5 and int(cough)==1:
-        print(name)
-        total+=1
-print(total)
